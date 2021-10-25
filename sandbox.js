7084162f132562055d43debd9c4378f43efb9684
Gino33Golden <62106179+Gino33Golden@users.noreply.github.com>
2021-10-25 09:27:33 -0400

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,53 @@
+//console.log(1);
+//console.log(2);
+
+
+//alert('hello, world');
+
+/* let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+var score = 75;
+console.log(score); */
+
+//strings
+console.log('hello, world');
+
+let email = 'mario@rgenetninja.com.uk';
+console.log(email);
+
+//string conection
+let firstName = firstNamr + ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//string length
+console.log (fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+//common string methods
+
+let email = 'mario@thenetninja.com.uk';
+
+//let result = email.lastIndexOf('n');
+
+let result = email.slice(0, 5);
+console.log(result);
